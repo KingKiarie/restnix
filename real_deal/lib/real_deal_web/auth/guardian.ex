4dad734136f777ac53bd6eac101bf1cb8fd51324
KingKiarie <kiarie7mbugua@gmail.com>
2025-06-17 11:42:42 +0300

    chore: updated Guardian Logic

---
+++ real_deal/lib/real_deal_web/auth/guardian.ex
@@ -0,0 +1,26 @@
+defmodule RealDealWeb.Auth.Guardian do
+  use Guardian, otp_app: :real_deal
+  alias RealDeal.Accounts
+
+  def subject_for_token(%{id: id}, _claims) do
+    sub = to_string(id)
+    {:ok, id}
+  end
+
+  def subject_for_token(_, _) do
+    {:error, :no_id_provided}
+  end
+
+  def resource_from_claims(%{"sub"=> id}) do
+    case Accounts.get_account!(id) do
+      nil -> {:error, :not_found}
+      resource ->{:ok, resource}
+    end
+  end
+
+  def resource_from_claims(_) do
+    {:error, :no_id_provided}
+  end
+
+
+end
